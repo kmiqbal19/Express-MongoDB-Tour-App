65fdfc6b8b75004888006bfddbe5cfa963167815
kmiqbal19 <km.iqbal01@gmail.com>
2022-04-07 04:58:30 +0200

    write the data by post on database

--- app.js
+++ app.js
@@ -19,8 +19,23 @@ app.get('/api/v1/tours', (req, res) => {
 });
 // POST
 app.post('/api/v1/tours', (req, res) => {
-  console.log(req.body);
-  res.send('DONE!!');
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tours: tours,
+        },
+      });
+    }
+  );
+  // res.send('DONE!!');
 });
 // Listen to Server
 const port = 3000;
