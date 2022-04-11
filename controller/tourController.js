1feb46ca8e0c16f2dcf159d01f6ab55f481b335e
kmiqbal19 <km.iqbal01@gmail.com>
2022-04-11 22:26:48 +0200

    Refactored the tourcontroller all code by transfering all query methods into a separate class named APIFeatures

--- controller/tourController.js
+++ controller/tourController.js
@@ -1,4 +1,5 @@
-const Tour = require('./../models/tourModel');
+const Tour = require('../models/tourModel');
+const APIFeatures = require('../utils/APIFeatures');
 // Middlewares
 exports.alliasTopTours = (req, res, next) => {
   req.query.limit = 5;
@@ -6,74 +7,29 @@ exports.alliasTopTours = (req, res, next) => {
   req.query.fields = 'name,price,description,difficulty,ratingsAverage';
   next();
 };
+
 // ROUTE HANDLERS
 exports.getAllTours = async (req, res) => {
   try {
-    // BUILD THE QUERY
-    // 1A) Filtering
-    const queryObj = { ...req.query };
-    const excludedFields = ['page', 'sort', 'limits', 'fields'];
-    excludedFields.forEach((el) => delete queryObj[el]);
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
 
-    // 1B) Advanced Filtering
-    // For mongoDB query object should be like
-    // {difficulty: 'medium' , price : {$lt: 500}}
-    // We got {difficulty: 'medium' , price : {lt: 500}} So the difference is the $ sign
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-
-    // MongoDB Method
-    let query = Tour.find(JSON.parse(queryStr));
-
-    // 2) Sorting
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      // console.log(sortBy);
-      // sort('price ratingsAverage')
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort('-createdAt name');
-    }
-    // 3) Field Limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query.select(fields);
-    } else {
-      query = query.select('-__v');
-    }
-    // 4) Pagination
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-
-    // console.log(typeof page, page, typeof limit, limit, typeof skip, skip);
-
-    query = query.skip(skip).limit(limit);
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw new Error('This page does not exists 🥵');
-    }
-    // EXECUTE THE QUERY
-    // query.find().sort().select().skip().limit() // All possible bcoz everyone returns a Mongoose Query Object. So we can chain them.
-    const tours = await query;
-    // Mongoose Query Methods (See doc for other methods e.g lte, gte)
-    // const query = Tour.find()
-    //   .where('duration')
-    //   .equals(5)
-    //   .where('difficulty')
-    //   .equals('easy');
     // SEND THE RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
       data: {
-        tours: tours,
-      },
+        tours: tours
+      }
     });
   } catch (err) {
     res.status(400).json({
       status: 'failed',
-      message: err,
+      message: err
     });
   }
 };
@@ -84,13 +40,13 @@ exports.getTour = async (req, res) => {
     res.status(200).json({
       status: 'success',
       data: {
-        tour: tour,
-      },
+        tour: tour
+      }
     });
   } catch (err) {
     res.status(404).json({
       status: 'failed',
-      message: err,
+      message: err
     });
   }
 };
@@ -102,13 +58,13 @@ exports.createTours = async (req, res) => {
     res.status(201).json({
       status: 'success',
       data: {
-        tour: newTour,
-      },
+        tour: newTour
+      }
     });
   } catch (err) {
     res.status(400).json({
       status: 'fail',
-      message: err,
+      message: err
     });
   }
 };
@@ -116,33 +72,34 @@ exports.updateTours = async (req, res) => {
   try {
     const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
-      runValidators: true,
+      runValidators: true
     });
     res.status(200).json({
       status: 'SUCCESS',
       data: {
-        tour: tour,
-      },
+        tour: tour
+      }
     });
   } catch (err) {
     res.status(404).json({
       status: 'failed',
-      message: err,
+      message: err
     });
   }
 };
+
 exports.deleteTours = async (req, res) => {
   try {
     await Tour.findByIdAndDelete(req.params.id);
     res.status(204).json({
       status: 'success',
       message: `${req.params.id} document has been deleted!`,
-      data: null,
+      data: null
     });
   } catch (err) {
     res.status(404).json({
       status: 'failed',
-      message: err,
+      message: err
     });
   }
 };
