68f88beb71d31cf5246738267048e08856248a3e
kmiqbal19 <km.iqbal01@gmail.com>
2022-04-13 15:22:00 +0200

    Implemented error handling by creating a AppError classs and errorcontroller

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const app = express();
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controller/errorController');
+
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 // 1) MIDDLEWARES
@@ -26,9 +29,9 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 // Error Handling for Invalid Url by using Middleware
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'FAILED',
-    message: `Couldn't find ${req.originalUrl} in this server!`
-  });
+  next(
+    new AppError(`Couldn't find this ${req.originalUrl} in this server!`, 404)
+  );
 });
+app.use(globalErrorHandler);
 module.exports = app;
