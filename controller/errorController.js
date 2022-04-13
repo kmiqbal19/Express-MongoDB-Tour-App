28ce7c83c959ba2acfc5bad6580d6883c07688c3
kmiqbal19 <km.iqbal01@gmail.com>
2022-04-14 00:17:20 +0200

    handled  mongoose validation error for production mode

--- controller/errorController.js
+++ controller/errorController.js
@@ -1,7 +1,20 @@
 const AppError = require('../utils/appError');
 
-const handleCastError = err => {
+const handleCastErrorDB = err => {
   const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+const handleDuplicateFields = err => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+
+  const message = `Duplicate field value: ${value} Please use another value!`;
+
+  return new AppError(message, 400);
+};
+const handleValidationErrorDB = err => {
+  const errors = Object.values(err.errors).map(el => el.message);
+  const message = `Invalid input data: ${errors.join('. ')}`;
+
   return new AppError(message, 400);
 };
 const sendErrorDev = (res, err) => {
@@ -41,7 +54,13 @@ module.exports = (err, req, res, next) => {
     let error = Object.assign(err);
 
     if (error.name === 'CastError') {
-      error = handleCastError(error);
+      error = handleCastErrorDB(error);
+    }
+    if (error.code === 11000) {
+      error = handleDuplicateFields(error);
+    }
+    if (error.name === 'ValidationError') {
+      error = handleValidationErrorDB(error);
     }
     sendErrorProd(res, error);
   }
