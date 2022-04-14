1f4e405aceb2d348c1b9a54c89f5756fed722057
kmiqbal19 <km.iqbal01@gmail.com>
2022-04-14 02:53:00 +0200

    Implemented unhandled and uncaught errors

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 // Configure the PROCESS ENV Variables
 const dotenv = require('dotenv');
+
+process.on('uncaughtException', err => {
+  console.log(err.name, err.message);
+  console.log('Uncaught Exception! 💥 Shutting down....');
+
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 // console.log(process.env);
 // MONGOOSE for connecting DB
 const mongoose = require('mongoose');
+
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
@@ -19,10 +28,18 @@ mongoose
     // console.log(con.connections);
     console.log('You have successfully logged in to MongoDB!');
   });
-
+// .catch(err => console.log(err));
 const app = require('./app');
 // START SERVER
 const port = process.env.PORT || 3000;
-app.listen(port, '127.0.0.1', () => {
+const server = app.listen(port, '127.0.0.1', () => {
   console.log(`App is running on port ${port}...`);
+});
+// Safety Net
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message);
+  console.log('Unhandled rejection! Shutting down....');
+  server.close(() => {
+    process.exit(1);
+  });
 });
