a73636ba20a612a6a3e0491819a006d2dc03924b
kmiqbal19 <km.iqbal01@gmail.com>
2022-04-18 01:51:04 +0200

    Implemented cookies with JWT

--- controller/authController.js
+++ controller/authController.js
@@ -13,6 +13,16 @@ const signToken = id => {
 };
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+
+    httpOnly: true
+  };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  res.cookie('jwt', token, cookieOptions);
+  user.password = undefined;
   res.status(statusCode).json({
     status: 'success',
     token: token,
@@ -29,6 +39,7 @@ exports.signup = catchAsync(async (req, res, next) => {
   //   passwordConfirm: req.body.passwordConfirm
   // });
   const newUser = await User.create(req.body);
+
   createSendToken(newUser, 201, res);
 });
 exports.login = catchAsync(async (req, res, next) => {
