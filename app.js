9a0676c7228880488283555b23c609f249ea66c4
kmiqbal19 <km.iqbal01@gmail.com>
2022-04-18 22:48:38 +0200

    Implemented data sanitization with mongo sanitizer and xss , also implemented hpp for preventionof parameter pollution

--- app.js
+++ app.js
@@ -1,27 +1,54 @@
 const express = require('express');
 
 const app = express();
+const helmet = require('helmet');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controller/errorController');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 // 1) GLOBAL MIDDLEWARES
-// console.log(app.get('env'));
+
+// Set Security HTTP Headers
+app.use(helmet());
+// Development Logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+// Limit requests from same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, Please try again in an hour!'
 });
 app.use('/api', limiter);
-app.use(express.json());
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+// Data Sanitization against NoSQL query injection
+app.use(mongoSanitize());
+// Data sanitization against XSS
+app.use(xss());
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  })
+);
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
-
+// Test Middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
 
