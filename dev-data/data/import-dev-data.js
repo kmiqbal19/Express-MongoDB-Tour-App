602706bbc20c51eb0cd210377496e890e431b1c3
kmiqbal19 <km.iqbal01@gmail.com>
2022-04-22 23:55:35 +0200

    Imported bunch of new user data and reviews in mongoDB using import-dev-data.js

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -6,6 +6,8 @@ dotenv.config({ path: '../../config.env' });
 // console.log(process.env.DATABASE);
 const mongoose = require('mongoose');
 const Tour = require('../../models/tourModel');
+const User = require('../../models/userModel');
+const Review = require('../../models/reviewModel');
 
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
@@ -23,10 +25,16 @@ mongoose
   });
 // READ JSON FILE
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
+);
 // IMPORT DATA IN DB
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log('Data has imported successfully....');
   } catch (err) {
     console.log(err);
@@ -36,6 +44,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log('Data has been deleted successfully....');
   } catch (err) {
     console.log(err);
--- models/userModel.js
+++ models/userModel.js
@@ -48,20 +48,20 @@ const userSchema = new mongoose.Schema({
   }
 });
 // Middleware prehook
-userSchema.pre('save', async function(next) {
-  // Only run this function if password was actually modified
-  if (!this.isModified('password')) return next();
-  // Hash the password with cost parameter of 12
-  this.password = await bcryptjs.hash(this.password, 12);
-  // Delete the password confirm field
-  this.passwordConfirm = undefined;
-  next();
-});
-userSchema.pre('save', function(next) {
-  if (!this.isModified('password') || this.isNew) return next();
-  this.passwordChangedAt = Date.now() - 1000;
-  next();
-});
+// userSchema.pre('save', async function(next) {
+//   // Only run this function if password was actually modified
+//   if (!this.isModified('password')) return next();
+//   // Hash the password with cost parameter of 12
+//   this.password = await bcryptjs.hash(this.password, 12);
+//   // Delete the password confirm field
+//   this.passwordConfirm = undefined;
+//   next();
+// });
+// userSchema.pre('save', function(next) {
+//   if (!this.isModified('password') || this.isNew) return next();
+//   this.passwordChangedAt = Date.now() - 1000;
+//   next();
+// });
 
 userSchema.pre(/^find/, function(next) {
   // this points to the current query
