ecb3220311daf86549e3b35bf4aa8f71f38b268c
kmiqbal19 <km.iqbal01@gmail.com>
2022-04-23 16:36:53 +0200

    Calculated averae rating on tour after updating or delting reviews with aggregate pipline

--- controller/handlerFactory.js
+++ controller/handlerFactory.js
@@ -67,7 +67,8 @@ exports.getAll = Model =>
       .sort()
       .limitFields()
       .paginate();
-    const doc = await features.query.explain();
+    // const doc = await features.query.explain();
+    const doc = await features.query;
 
     // SEND THE RESPONSE
     res.status(200).json({
