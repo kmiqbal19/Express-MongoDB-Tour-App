f057de16928f08238c911402ff4c6170de2dbd79
kmiqbal19 <km.iqbal01@gmail.com>
2022-04-29 03:07:24 +0200

    Finished establishing map in the tour endpoint with mapbox-gl

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -4,4 +4,44 @@ console.log('Hello from the client side');
 
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
 
-console.log(locations);
+mapboxgl.accessToken =
+  'pk.eyJ1Ijoia2lxYmFsMDEiLCJhIjoiY2wyamoxNHE0MHM0eDNubzdyY2ZqeHRkeiJ9.plR_6jIIVzVI9FzpMnU5Kw';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/kiqbal01/cl2jk86gi002414qq0aw3whmn',
+  scrollZoom: false
+  //   center: [-118.113491, 34.111745],
+  //   zoom: 10,
+  //   interactive: false
+});
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach(loc => {
+  // Create Marker
+  const el = document.createElement('div');
+  el.className = 'marker';
+  // Add Marker
+  new mapboxgl.Marker({
+    element: el,
+    anchor: 'bottom'
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+  // Add popup
+  new mapboxgl.Popup({
+    offset: 30
+  })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+    .addTo(map);
+  // Extend map bounds to include current location
+  bounds.extend(loc.coordinates);
+});
+map.fitBounds(bounds, {
+  padding: {
+    top: 150,
+    bottom: 150,
+    left: 100,
+    right: 100
+  }
+});
