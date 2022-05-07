d332d4e51a09302cb8046fd26db40d3d2a8f8096
kmiqbal19 <km.iqbal01@gmail.com>
2022-05-07 22:58:08 +0200

    minor

--- public/js/index.js
+++ public/js/index.js
@@ -3,11 +3,12 @@ import '@babel/polyfill';
 import { login, logout } from './login';
 import { displayMap } from './mapbox';
 console.log('Hello from parcel');
-
+import { updateData } from './updateSettings';
 // DOM ELEMENTS
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
+const userDataForm = document.querySelector('.form-user-data');
 // DELEGATION
 if (mapBox) {
   const locations = JSON.parse(mapBox.dataset.locations);
@@ -23,3 +24,12 @@ if (loginForm) {
   });
 }
 if (logOutBtn) logOutBtn.addEventListener('click', logout);
+// if (userDataForm) {
+//   userDataForm.addEventListener('submit', e => {
+//     e.preventDefault();
+//     const email = document.getElementById('email').value;
+//     const name = document.getElementById('name').value;
+//     console.log(name, email);
+//     updateData(name, email);
+//   });
+// }
